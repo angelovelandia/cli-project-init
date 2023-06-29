b910f648cbb3028a53db67c137c0779a6ee679c8
Angelo Velandia <angelo.velandiar@grupomok.com>
2023-06-28 23:00:11 -0500

    Ajustes generales

--- index.js
+++ index.js
@@ -5,12 +5,54 @@ import figlet from "figlet";
 import inquirer from "inquirer";
 import fs from "fs";
 
-import {
-  PATH,
-  QUERYS,
-  EXPORT_DEFAULT,
-  EXPORT_DEFAULT_ROUTER,
-} from "./config/index.js";
+const PATH = process.cwd().replace("\\", "/");
+
+const QUERYS = [
+  {
+    name: "template",
+    type: "list",
+    message: "Seleccionar plantilla: ",
+    choices: ["ReactJS"],
+  },
+  {
+    name: "router",
+    type: "input",
+    message: "Crear router true/false: ",
+    default: "true",
+  },
+  {
+    name: "components",
+    type: "input",
+    message: "Escriba los componentes separados por coma (Header, Footer): ",
+    default: "Header, Banner, Footer, Loader, Modals",
+  },
+  {
+    name: "ext",
+    type: "list",
+    message: "Selecionar extension de componentes:",
+    choices: [".js", ".jsx"],
+    default: ".js",
+  },
+  {
+    name: "extcss",
+    type: "list",
+    message: "Selecionar extension de archivos de estilos:",
+    choices: [".css", ".scss"],
+    default: ".css",
+  },
+];
+
+const EXPORT_DEFAULT = `export default function [component]() {
+    return (
+        <div>[component]</div>
+    )
+}`;
+
+const EXPORT_DEFAULT_ROUTER = `export default function Router() {
+    return (
+        <div>Router</div>
+    )
+}`;
 
 const log = console.log;
 
